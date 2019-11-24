4528c61817e9aa4379bec22e8dcc2d3f61076381
Amakhin <amakhinruslan@gmail.com>
2019-11-24 23:32:33 +0200

    Working with interface

---
+++ PassDataProject/SecondViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SecondViewController.swift
+//  PassDataProject
+//
+//  Created by Amakhin Ruslan on 24.11.2019.
+//  Copyright © 2019 Amakhin Ruslan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func sendPressed(button: UIButton) {
+        
+    }
+}
--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -9,6 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func sendPressed(button: UIButton) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
