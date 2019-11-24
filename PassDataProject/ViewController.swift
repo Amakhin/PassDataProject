17c7404e86b5e07fa1423cafc8dac119834ebc8b
Amakhin <amakhinruslan@gmail.com>
2019-11-25 00:26:40 +0200

    Unwind segue

--- PassDataProject/ViewController.swift
+++ PassDataProject/ViewController.swift
@@ -12,9 +12,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var returnLabel: UILabel!
     
     @IBAction func sendPressed(button: UIButton) {
         performSegue(withIdentifier: "detailSegue", sender: nil)
+    }
+    
+    @IBAction func unwindToMainScreen(segue: UIStoryboardSegue) {
+        guard segue.identifier == "unwindSegue" else { return }
+        guard let svc = segue.source as? SecondViewController else { return }
+        self.returnLabel.text = svc.label.text
     }
 
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
