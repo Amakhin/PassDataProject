97c8e1a8c1ee5e1e66f8fa0bc571f5fc7fadc3ef
Amakhin <amakhinruslan@gmail.com>
2019-11-25 00:00:10 +0200

    Segues

--- PassDataProject/SecondViewController.swift
+++ PassDataProject/SecondViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    var login: String!
 
     @IBOutlet weak var label: UILabel!
     
     @IBAction func sendPressed(button: UIButton) {
         
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        guard let login = login else { return }
+        label.text = "Hello, \(login)"
     }
 }
